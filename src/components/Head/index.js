6c1b19afdee483d45f7be5ff765e5193f90977c2
stscoundrel <sampo@pixels.fi>
2021-05-02 12:34:11 +0300

    Add manifest & icons

--- src/components/Head/index.js
+++ src/components/Head/index.js
@@ -11,10 +11,8 @@ export default function Head() {
     <NextHead>
       <link rel='icon' href='/favicon.ico' />
         <title>{title}</title>
-        <link rel='icon' type='image/png' sizes='48x48' href='/favicon-48x48.png' />
-        <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
-        <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
-        <link rel="apple-touch-icon" href="/favicon-96x96.png"></link>
+        <link rel='icon' type='image/png' sizes='48x48' href='/icon-72x72.png' />
+        <link rel="apple-touch-icon" href="/icon-96x96.png"></link>
         <meta
           name='description'
           content={description}
