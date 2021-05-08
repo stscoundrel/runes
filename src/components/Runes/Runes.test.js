6f1f2f415b31d144a3ca28071acdde650f66702c
stscoundrel <sampo@pixels.fi>
2021-05-08 10:43:01 +0300

    Tests: add unit tests of rune component

--- src/components/Runes/Runes.test.js
+++ src/components/Runes/Runes.test.js
@@ -13,4 +13,39 @@ describe('Runes component', () => {
     const tree = renderer.create(<Runes />).toJSON()
     expect(tree).toMatchSnapshot()
   })
+
+  test('Typing text to input produces transcriptions', async () => {
+    let tree
+
+    await renderer.act(async () => {
+      tree = renderer.create(
+        <Runes />,
+      )
+    })
+
+    /**
+     * Assert content is empty at start
+     */
+    expect(tree.root.findByType('textarea').children).toEqual([])
+
+    /**
+     * Trigger event to input field.
+     */
+    await renderer.act(async () => {
+      const mockEvent = jest.fn()
+      mockEvent.target = { value: 'Some text' }
+      await tree.root.findByType('textarea').props.onChange(mockEvent)
+    })
+
+    /**
+     * Assert content was changed
+     */
+    expect(JSON.stringify(tree)).toContain('Some text')
+
+    /**
+     * Assert transcriptions were provided
+     */
+    expect(JSON.stringify(tree)).toContain('ᛋᚢᛘᛁ:ᛏᛁᛋᛏ')
+    expect(JSON.stringify(tree)).toContain('ᛋᛟᛗᛖ:ᛏᛖᛋᛏ')
+  })
 })
