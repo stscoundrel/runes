0cad1f13d9fb950c6840dd73592d49ded9ebda3b
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2022-07-02 18:06:46 +0300

    TypeScript configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,29 @@
 module.exports = {
   extends: [
     'airbnb-base',
+    'next/core-web-vitals',
     'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
   env: {
     node: true,
-    es6: true,
+    es2020: true,
     jest: true,
     'cypress/globals': true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
   },
   globals: {
     document: true,
     'jest/globals': true,
   },
   plugins: [
-    'import',
-    'react',
-    'react-hooks',
     'jest',
     'cypress',
+    '@typescript-eslint',
   ],
+  parser: '@typescript-eslint/parser',
   settings: {
     react: {
       version: '17.0.1',
@@ -35,5 +39,9 @@ module.exports = {
     'no-console': 0,
     'react/prop-types': 0,
     'react/react-in-jsx-scope': 0,
+    'import/no-extraneous-dependencies': 0,
+    'import/extensions': 0,
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': ['error'],
   },
 };
--- setupTests.js
+++
@@ -1,3 +0,0 @@
- // Force certain date to keep test deterministic.
- jest.useFakeTimers('modern');
- jest.setSystemTime(new Date(2021, 1, 1, 1, 1, 1));
